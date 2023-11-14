91dbb3f36295a43a7cda39f33e77090bf1bb2121
Aamaxi <112830001+Aamaxi@users.noreply.github.com>
2023-11-14 12:09:39 +0100

    added js code

---
+++ script.js
@@ -0,0 +1,11 @@
+const fadeElement = document.querySelector("nav");
+
+function scrollOpacity () {
+  let scrollPosition = document.scrollY;
+  let maxScroll = 10;
+  if (maxScroll > scrollPosition) {
+    
+  }
+}
+
+document.addEventListener("scroll", "scrollOpacity");
